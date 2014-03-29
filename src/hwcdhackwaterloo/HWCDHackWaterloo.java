e5e61fd5dd36feaab55cc2cec03e6d55af0603b7
AnthonyCalandra <AnthonyCalandra@172.22.11.149>
2014-03-28 23:35:13 -0400

    Initial commit.

---
+++ src/hwcdhackwaterloo/HWCDHackWaterloo.java
@@ -0,0 +1,12 @@
+
+package hwcdhackwaterloo;
+
+public class HWCDHackWaterloo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}

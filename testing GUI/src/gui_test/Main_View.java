99b334e9ca0c70e381597f28ac47eadcd1a7ead0
Neil de Vries <neil.b.devries@gmail.com>
2014-03-29 06:47:30 -0400

    final update on personal GUI attempts

--- testing GUI/src/gui_test/Main_View.java
+++ testing GUI/src/gui_test/Main_View.java
@@ -3,15 +3,10 @@ package gui_test;
 import java.awt.Color;
 
 import javax.swing.*;
-import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;

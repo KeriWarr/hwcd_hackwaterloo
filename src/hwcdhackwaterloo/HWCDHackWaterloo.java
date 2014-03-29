9e9ad4c928d5afb3ee27e5f3cc85e1d5ba644fe8
AnthonyCalandra <AnthonyCalandra@172.22.11.149>
2014-03-29 02:21:31 -0400

    Removed test example.

--- src/hwcdhackwaterloo/HWCDHackWaterloo.java
+++ src/hwcdhackwaterloo/HWCDHackWaterloo.java
@@ -7,7 +7,7 @@ public class HWCDHackWaterloo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String[] keys = new String[] {"data", "temperature_current_c", "ok"};
+        String[] keys = new String[] {"data", "temperature_current_c"};
         String ret = UWAPI.get("/weather/current", keys);
         System.out.println(ret);
     }
